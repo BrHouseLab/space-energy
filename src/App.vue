61d614101794bac9809a5fff744769d5a2a7a22f
Anton Naidenov <brhouselab@gmail.com>
2020-03-15 14:42:47 +0300

    merging

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home1</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
@@ -22,7 +22,7 @@
 }
 
 #nav a {
-  font-weight: bold;
+  font-weight: bolder;
   color: #2c3e50;
 }
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Argon from '@/plugins/argon-kit'
+Vue.use(Argon);
 
 Vue.config.productionTip = false
 
